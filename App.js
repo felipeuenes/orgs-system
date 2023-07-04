98d10cd3d8a772b247fac1699b2a49abdafa2cee
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-04 16:32:59 -0300

    qee

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textos}>Começando no React Native!</Text>
+      <Text style={styles.textos}> Cuida!!!!! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textos: {
+      color: '#fff'
   },
 });
