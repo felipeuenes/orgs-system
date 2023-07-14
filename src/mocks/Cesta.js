a45051d4de91867e945b86b0057e736629ee1b08
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-14 11:45:22 -0300

    add lista

--- src/mocks/Cesta.js
+++ src/mocks/Cesta.js
@@ -1,5 +1,11 @@
 import logo from '../../assets/logo.png'
 
+import tomate from '../../assets/frutas/Tomate.png'
+import brócolis from '../../assets/frutas/Brócolis.png'
+import batata from '../../assets/frutas/Batata.png'
+import pepino from '../../assets/frutas/Pepino.png'
+import abobora from '../../assets/frutas/Abóbora.png'
+
 const cesta = {
 
     topo: {
@@ -13,6 +19,31 @@ const cesta = {
         preco: 'R$ 40,00',
         botao: 'Comprar',
     },
+    itens: {
+        titulo: 'Itens da cesta',
+        lista: [
+            {
+                nome: 'Tomate',
+                image: tomate
+            },
+            {
+                nome: 'Brócolis',
+                image: brócolis,
+            },
+            {
+                nome: 'Batata',
+                image: batata
+            },
+            {
+                nome: 'Pepino',
+                image: pepino
+            },
+            {
+                nome: 'Abóbora',
+                image: abobora
+            }
+        ]
+    }
 
 }
 
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView ,StyleSheet, View } from "react-native";
 
 
 
 
 import Topo from "./Components/Topo";
 import Detalhes from "./Components/Detalhes";
+import Itens from "./Components/itens";
 
 
 
-export function Cesta({ topo, detalhes }){
+export function Cesta({ topo, detalhes, itens }){
 
     return(
-        <>
+       <ScrollView>
+
 
         <Topo {...topo}/>
         
         <View style={estilos.cesta}>
             <Detalhes {...detalhes} />
+            <Itens {...itens}/>
         </View>
-        </>
+        
+        </ScrollView>
+        
     )
 }
 
